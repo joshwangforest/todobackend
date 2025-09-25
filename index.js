4854d4248c4189d0405eedde2d082578c4791750
josh wang <josh.wang@forestcorp.io>
2025-09-25 21:29:26 +0900

    Fix Heroku deployment issues

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // CORS 설정 - 개발 환경에서 모든 origin 허용
 const corsOptions = {
